ce054e789d064930242e8e7f3f6d5f54273b9231
luorenjie <lrj193927@gmail.com>
2021-04-21 19:02:34 -0400

    init commit

---
+++ src/test/java/org/northernjay/jenkins_test/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.northernjay.jenkins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
